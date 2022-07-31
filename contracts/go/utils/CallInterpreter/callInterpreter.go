f01031237fd0b0a973752955f1671e971c08499d
Lorenzo Kramer <lorenzokramertje@gmail.com>
2022-08-01 01:42:12 +0200

    Created a command line call interpreter

---
+++ contracts/go/utils/CallInterpreter/callInterpreter.go
@@ -0,0 +1,75 @@
+package callintrpr
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+func Interpret(contract interface{}, args []string) {
+	methodName := args[0]
+	methodArguments := args[1:]
+
+	method := reflect.ValueOf(contract).MethodByName(methodName)
+
+	if !method.IsValid() {
+		os.Stdout.WriteString(fmt.Sprintf("CallInterpreter: Method \"%v\" does not exist\n", methodName))
+		os.Exit(1)
+	}
+
+	methodType := method.Type()
+
+	in := make([]reflect.Value, methodType.NumIn())
+
+	if len(in) != len(methodArguments) {
+		os.Stdout.WriteString(fmt.Sprintf("CallInterpreter: Expected %v arguments received %v\n", len(in), len(methodArguments)))
+		os.Exit(1)
+	}
+
+	for i := 0; i < methodType.NumIn(); i++ {
+		t := methodType.In(i)
+		arg := methodArguments[i]
+
+		switch t.Kind() {
+		default:
+		case reflect.String:
+			in[i] = reflect.ValueOf(arg)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			n, err := strconv.ParseInt(arg, 10, 64)
+
+			if err != nil {
+				os.Stderr.WriteString(fmt.Sprintf("CallInterpreter: Expected \"%v\" to be of type %v\n", arg, t))
+				os.Exit(1)
+			}
+
+			in[i] = reflect.ValueOf(n).Convert(t)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			n, err := strconv.ParseUint(arg, 10, 64)
+
+			if err != nil {
+				os.Stderr.WriteString(fmt.Sprintf("CallInterpreter: Expected \"%v\" to be of type %v\n", arg, t))
+				os.Exit(1)
+			}
+
+			in[i] = reflect.ValueOf(n).Convert(t)
+		case reflect.Bool:
+			b, err := strconv.ParseBool(arg)
+
+			if err != nil {
+				os.Stderr.WriteString(fmt.Sprintf("CallInterpreter: Expected \"%v\" to be of type %v\n", arg, t))
+				os.Exit(1)
+			}
+
+			in[i] = reflect.ValueOf(b).Convert(t)
+		}
+	}
+
+	if methodType.NumOut() > 0 {
+		fmt.Printf("OUT=%v\n", method.Call(in))
+		os.Exit(0)
+	}
+
+	method.Call(in)
+	os.Exit(0)
+}
---
+++ contracts/go/utils/CallInterpreter/go.mod
@@ -0,0 +1,3 @@
+module qanx.space/qanx-contracts/go/utils/CallInterpreter
+
+go 1.18
