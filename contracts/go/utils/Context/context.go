a119aad6d99a6e596b6026da22c7f6a2a15f9e6f
Lorenzo Kramer <lorenzokramertje@gmail.com>
2022-08-01 16:49:31 +0200

    Now checks if Sender environment variable is available

--- contracts/go/utils/Context/context.go
+++ contracts/go/utils/Context/context.go
@@ -4,5 +4,12 @@ import "os"
 
 // Retrieves the sender of the transaction
 func Sender() string {
-	return os.Getenv("SENDER")
+	sender := os.Getenv("SENDER")
+
+	if len(sender) == 0 {
+		os.Stderr.WriteString("Context: Sender is not known\n")
+		os.Exit(1)
+	}
+
+	return sender
 }
