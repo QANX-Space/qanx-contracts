c14c5f09408cd7b0c6eabed53f2dc6e3ba080a2d
Lorenzo Kramer <lorenzokramertje@gmail.com>
2022-08-01 14:54:25 +0200

    Added example for QRC721

--- contracts/go/QRC721/qrc721.go
+++ contracts/go/QRC721/qrc721.go
@@ -1,3 +1,24 @@
+/*
+## Example
+
+```go
+
+package main
+
+import (
+	"os"
+
+	qrc721 "qanx.space/qanx-contracts/go/QRC721"
+	callintrpr "qanx.space/qanx-contracts/go/utils/CallInterpreter"
+)
+
+func main() {
+	token := qrc721.NewQRC721("Example", "XMPL", "https://example.com/")
+
+	callintrpr.Interpret(token, os.Args[1:])
+}
+
+```*/
 package qrc721
 
 import (
