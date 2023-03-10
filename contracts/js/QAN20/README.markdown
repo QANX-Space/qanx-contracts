add3f2d45f346fb11ba20561575642afeaada678
Lorenzo Kramer <lorenzokramertje@gmail.com>
2023-03-10 18:59:28 +0100

    QAN20 Javascript version

---
+++ contracts/js/QAN20/README.markdown
@@ -0,0 +1,103 @@
+# qan721
+
+## Example
+
+```js
+const callintrpr = require("./utils/CallInterpreter/callInterpreter");
+
+const QAN20 = require("./QAN20/qan20");
+
+const qan20 = new QAN20("Example", "XMPL", 18n);
+
+callintrpr.interpret(qan20, process.argv.slice(2));
+```
+
+## Usage
+
+#### Constructor
+
+```js
+const qan20 = new QAN20("Example", "XMPL", 18n);
+```
+
+Creates the QAN721 smart contract
+
+#### Allowance
+
+```go
+qan20.allowance(owner, spender);
+```
+
+Retrieve the remaining amount of tokens allowed to spend by spender
+
+#### Approve
+
+```go
+qan20.approve(spender, amount);
+```
+
+Sets amount as the allowance of spender over the caller's tokens
+
+#### Balance Of
+
+```go
+qan20.balanceOf(owner);
+```
+
+Retrieve the balance of owner
+
+#### Decimals
+
+```go
+qan20.decimals();
+```
+
+Retrieve the decimal amount
+
+#### Mint
+
+```go
+qan20.mint(to, amount);
+```
+
+Mints the amount of tokens and transfers it to "to"
+
+#### Name
+
+```go
+qan20.name();
+```
+
+Retrieve the name
+
+#### Symbol
+
+```go
+qan20.symbol();
+```
+
+Retrieve the symbol/ticker
+
+#### Total Supply
+
+```go
+qan20.totalSupply();
+```
+
+Retrieve the total supply
+
+#### Transfer
+
+```go
+qan20.transfer(to, amount);
+```
+
+Transfers tokens from sender to "to"
+
+#### Transfer From
+
+```go
+qan20.transferFrom(from, to, amount);
+```
+
+Transfers tokens from "from" to "to"
---
+++ contracts/js/QAN20/qan20.js
@@ -0,0 +1,107 @@
+const context = require("../utils/Context/context");
+const db = require("../utils/Database/database");
+
+module.exports = class QAN20 {
+  _name = "";
+  _symbol = "";
+  _decimals = 0n;
+
+  // Creates the QAN20 smart contract
+  constructor(name, symbol, decimals) {
+    this._name = name;
+    this._symbol = symbol;
+    this._decimals = decimals;
+  }
+
+  // Retrieve the name
+  name() {
+    return this._name;
+  }
+
+  // Retrieve the symbol/ticker
+  symbol() {
+    return this._symbol;
+  }
+
+  // Retrieve the decimal amount
+  decimals() {
+    return this._decimals;
+  }
+
+  // Retrieve the total supply
+  totalSupply() {
+    const n = BigInt(db.read("TOTAL_SUPPLY", 0));
+    return n;
+  }
+
+  // Retrieve the balance of owner
+  balanceOf(owner) {
+    owner = owner.toLowerCase();
+
+    const n = BigInt(db.read(`BALANCE_OF_${owner}`, 0));
+    return n;
+  }
+
+  // Transfers tokens from sender to "to"
+  transfer(to, amount) {
+    to = to.toLowerCase();
+
+    const sender = context.sender();
+
+    return transfer(sender, to, amount).bind(this);
+  }
+
+  // Retrieve the remaining amount of tokens allowed to spend by spender
+  allowance(owner, spender) {
+    owner = owner.toLowerCase();
+    spender = spender.toLowerCase();
+
+    const n = BigInt(db.read(`TOKEN_ALLOWANCE_${owner}_${spender}`, 0));
+    return n;
+  }
+
+  // Sets amount as the allowance of spender over the caller's tokens
+  approve(spender, amount) {
+    spender = spender.toLowerCase();
+
+    const sender = context.sender();
+    db.write(`TOKEN_ALLOWANCE_${sender}_${spender}`, amount);
+    return true;
+  }
+
+  // Transfers tokens from "from" to "to"
+  transferFrom(from, to, amount) {
+    from = from.toLowerCase();
+    to = to.toLowerCase();
+
+    const sender = context.sender();
+    const allowance = this.allowance(from, sender);
+
+    if (allowance < amount) {
+      process.stdout.write("QAN20: Insufficient allowance\n");
+      process.exit(1);
+    }
+
+    db.write(`TOKEN_ALLOWANCE_${from}_${sender}`, allowance - amount);
+
+    return transfer(from, to, amount).bind(this);
+  }
+
+  // Mints the amount of tokens and transfers it to "to"
+  mint(to, amount) {
+    db.write(`BALANCE_OF_${to}`, this.balanceOf(to) + amount);
+    db.write(`TOTAL_SUPPLY`, this.totalSupply() + amount);
+  }
+};
+
+function transfer(from, to, amount) {
+  const fromBalance = this.balanceOf(from);
+
+  if (fromBalance < amount) {
+    process.stdout.write("QAN20: Transfer amount exceeds balance\n");
+    process.exit(1);
+  }
+
+  db.write(`BALANCE_OF_${from}`, fromBalance - amount);
+  db.write(`BALANCE_OF_${to}`, this.balanceOf(to) + amount);
+}
