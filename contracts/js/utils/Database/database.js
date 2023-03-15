a4ace0325b9dc3a4f67327309ce3de68cae9f587
Lorenzo Kramer <lorenzokramertje@gmail.com>
2023-03-15 17:24:20 +0100

    Converted keys to sha256

--- contracts/js/utils/Database/database.js
+++ contracts/js/utils/Database/database.js
@@ -1,5 +1,23 @@
+const crypto = require("crypto");
+
+const keyMap = new Map();
+
+function sha256(key) {
+  if (keyMap.has(key)) {
+    return keyMap.get(key);
+  }
+
+  const hash = crypto.createHash("sha256").update(key).digest("hex");
+  const hashedKey = `0x${hash}`.toLowerCase();
+  keyMap.set(key, hashedKey);
+
+  return hashedKey;
+}
+
 module.exports.write = function write(key, value) {
-  process.env[`DB_${key}`] = value;
+  const hashedKey = sha256(key);
+
+  process.env[`DB_${hashedKey}`] = value;
 
   if (typeof value === "bigint" || typeof value === "number") {
     value = `0x${value.toString(16)}`;
@@ -7,15 +25,19 @@ module.exports.write = function write(key, value) {
     value = +value;
   }
 
-  process.stdout.write(`DBW=${key}=${value}\n`);
+  process.stdout.write(`DBW=${hashedKey}=${value}\n`);
 };
 
 module.exports.prune = function prune(key) {
+  key = sha256(key); // convert to 32 byte key
+
   process.stdout.write(`DBP=${key}\n`);
   process.env[`DB_${key}`] = "";
 };
 
 module.exports.read = function read(key, defaultsTo) {
+  key = sha256(key); // convert to 32 byte key
+
   const value = process.env[`DB_${key}`];
 
   if (!value && defaultsTo === undefined) {
